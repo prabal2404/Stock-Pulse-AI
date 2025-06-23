dfe6bdb12697eb7df4e6d753aa3bdf655c41e825
prabal2404 <prabal24042002@gmail.com>
2025-06-24 00:37:56 +0530

    Update app_ui.py

--- app_ui.py
+++ app_ui.py
@@ -135,7 +135,7 @@ st.markdown("""
 
 nse_stocks = load_nse_stocks()
 default_stock = "RELIANCE"
-if default_stock in options:
+if default_stock in nse_stocks:
     selected_stock = st.selectbox("Select Stock", nse_stocks, index=options.index(default_stock))
 else:
     selected_stock = st.selectbox("Select Stock", nse_stocks)
